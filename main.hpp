31756c84d123596781cd0c56d7e77060034d9865
Kyuwoong Lee <kwooler@gmail.com>
2024-03-25 16:54:47 -0700

    Update main.hpp

--- main.hpp
+++ main.hpp
@@ -1,80 +1,65 @@
 #include <iostream>
+#include <iomanip>
+#include <climits>
 using namespace std;
 
 //********************
 // Declare Function Prototypes
 //********************
-int getTwoValues(int, int);
 
-int getNextPrime(int);
-
-int getPrevPrime(int);
 // ******************************
 // Implement all your functions here
 // ******************************
-int getTwoValues(int begin,int end){
-    //do-while loop to ensure begin < end without having to waste extra lines
-    do {
-        //prompt #1
-        cout << "Enter the first number: " << endl;
-        cin >> begin;
-        //prompt #2
-        cout << "Enter the next number. This number must be smaller than " << begin << ": " << endl;
-        cin >> end;
-        //keeps the loop going
-        if(begin < end){
-            cout << "Your numbers are too low! Try again!" << endl;
-        }
-        //when begin > end
-        else{
-            cout << "Thank you for your cooperation!" << endl;
+
+void getTwoValues(int &, int &);
+int getNextPrime(int);
+int getPrevPrime(int);
+int isPrime(int);
+
+void getTwoValues(int &val1, int &val2)
+{
+    while (1)
+    {
+        cout << "Enter two values : ";
+        cin >> val1 >> val2;
+        if (val1 < val2)
             break;
-        }
-    } while (begin < end);
-    //unexpected error
-    cout << "The beginning number is: " << begin << endl;
-    return begin;
-    return end;
+    }
 }
 
-int getNextPrime(int begin){
-    // Finding prime numbers with space between begin and end
-    cout << "The beginning number is: " << begin << endl;
-    int i,j, prime;
-    for (i = 0; i <= j; i++){
-        for(j = 2; j <= (i/2); j++){
-            if (i % j == 0){
-                break;
-                }
-            }
-            //We found a prime number
-            if (j > (i/2)){
-                cout << i << " is a prime number\n";
-                prime = i;
-                if(i > begin){
-                    cout << i << " is the closest prime number to " << begin << "." << endl;
-                }
-            }
+int getNextPrime(int begin)
+{
+    int prime;
+    prime = begin + 1;
+    while (1)
+    {
+        if (isPrime(prime))
+            return prime;
+        prime += 1;
     }
-        // Return the number of prime numbers
-        return prime;
+    return INT_MAX;
 }
 
-int getPrevPrime(int end){
-    //code
-    int i,j, prime;
-    for (i = end - 1; ; i--){ // end - 1 to avoid checking the same number again
-        for(j = 2; j <= (i/2); j++){
-            if (i % j == 0){
-                break;
-            }
-        }
-        //We found a prime number
-        if (j > (i/2)){
-            cout << i << " is a prime number\n";
-            prime = i;
-            return prime; // return the first prime number found
+int isPrime(int number)
+{
+    for (int i = 2; i <= (number / 2); i++)
+    {
+        if (number % i == 0)
+        {
+            return 0;
         }
     }
-  return -1;
+    return 1;
+}
+int getPrevPrime(int end)
+{
+    int prime;
+    prime = end - 1;
+    while (1)
+    {
+        if (isPrime(prime))
+            return prime;
+        prime -= 1;
+    }
+    return INT_MIN;
 }
